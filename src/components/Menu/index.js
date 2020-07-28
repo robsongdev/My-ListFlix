f16bf8c5184624a5d37238d406393fce839c1cda
robsongdev linux <robsong369@gmail.com>
2020-07-28 11:13:47 -0300

    Atualização Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import Logo from '../../assets/img/Logo.png'
-import './Menu.css'
-import Button from '../Button/index'
+import {} from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../Button/index';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'; //SPA
 
 
 function Menu(){
     return(
         <nav className='Menu'>
-            <a href='/'>
-                <img className='Logo' src={Logo} alt = 'Robflix logo'/>
-            </a>
+            <Link to ='/'>
+                <img className='Logo' src={Logo} alt = 'My Listflix logo'/>
+            </Link>
 
             
-            <Button className='ButtonLink' href='/'>
+            <Button className='ButtonLink' to='/cadastro/video'>
                 Novo Vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/video/index'
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    <Route path='/' component = {Home} exact/> 
+    <Route path='/cadastro/video' component={CadastroVideo} />
+    <Route path='/cadastro/categoria' component={CadastroCategoria}/>
+    <Route component ={()=>(<div>Pagina 404</div>)} />
+  </Switch>
+  </BrowserRouter>,
+  
+  //<React.StrictMode>
+  //  <App />
+  //</React.StrictMode>,
   document.getElementById('root')
 );
 
