ec180bcffff187abac85494c8e378184944a8f97
robsongdev linux <robsong369@gmail.com>
2020-07-31 12:42:24 -0300

    teste final

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import styled, {css} from 'styled-components';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 
 const FormFieldWrapper = styled.div`
-  position: relative;
+  position: relative; 
   textarea {
     min-height: 150px;
   }
@@ -12,7 +13,6 @@ const FormFieldWrapper = styled.div`
 `;
 
 const Label = styled.label``;
-
 Label.Text = styled.span`
   color: #E5E5E5;
   height: 57px;
@@ -64,26 +64,69 @@ const Input = styled.input`
   `}
 `;
 
+function FormField({
+  label, type, name, value, onChange, suggestions,
+}) {
+  const fieldId = `id_${name}`;
+  const isTypeTextarea = type === 'textarea';
+  const tag = isTypeTextarea ? 'textarea' : 'input';
 
-function FormField ({label,type, name, value, onChange}){
-    const tag = type === 'textarea'? 'textarea' : 'input'
-    return(
-        <FormFieldWrapper>
-            <Label>
-                <Input
-                as = {tag}
-                type={type} 
-                value={value} 
-                name = {name}
-                //hasValue = {hasValue}
-                onChange = {onChange}/>
-                <Label.Text>
-                    {label}:
-                </Label.Text>
-                
-            </Label>
-        </FormFieldWrapper>
-    );
+  const hasValue = Boolean(value.length);
+  const hasSuggestions = Boolean(suggestions.length);
+
+  return (
+    <FormFieldWrapper>
+      <Label
+        htmlFor={fieldId}
+      >
+        <Input
+          as={tag}
+          id={fieldId}
+          type={type}
+          value={value}
+          name={name}
+          hasValue={hasValue}
+          onChange={onChange}
+          autoComplete={hasSuggestions ? 'off' : 'on'}
+          list={hasSuggestions ? `suggestionFor_${fieldId}` : undefined}
+        />
+        <Label.Text>
+          {label}
+          :
+        </Label.Text>
+        {
+          hasSuggestions && (
+            <datalist id={`suggestionFor_${fieldId}`}>
+              {
+              suggestions.map((suggestion) => (
+                <option value={suggestion} key={`suggestionFor_${fieldId}_option${suggestion}`}>
+                  {suggestion}
+                </option>
+              ))
+            }
+            </datalist>
+          )
+        }
+
+      </Label>
+    </FormFieldWrapper>
+  );
+}
+
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+  onChange: () => {},
+  suggestions: [],
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  suggestions: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default FormField;
--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -1,19 +1,32 @@
-import config from '../config'
+import config from '../config';
 
-const URL_CATEGORIES = `${config.URL_BACKEND_TOP}/categorias`
+const URL_CATEGORIES = `${config.URL_BACKEND_TOP}/categorias`;
 
-function getAllWitchVideos(){
-    return fetch(`${URL_CATEGORIES}?_embed=videos`)
-    .then(async(respostaDoServidor)=> {
+function getAll() {
+  return fetch(`${URL_CATEGORIES}`)
+    .then(async (respostaDoServidor) => {
+      if (respostaDoServidor.ok) {
+        const resposta = await respostaDoServidor.json();
+        return resposta;
+      }
 
-        if (respostaDoServidor.ok){
-            const resposta = await respostaDoServidor.json();
-            return resposta;
-        }
-
-        throw new Error ('Não foi possivel pegar os dados')
-    })
+      throw new Error('Não foi possível pegar os dados :(');
+    });
 }
+
+function getAllWithVideos() {
+  return fetch(`${URL_CATEGORIES}?_embed=videos`)
+    .then(async (respostaDoServidor) => {
+      if (respostaDoServidor.ok) {
+        const resposta = await respostaDoServidor.json();
+        return resposta;
+      }
+
+      throw new Error('Não foi possível pegar os dados :(');
+    });
+}
+
 export default {
-    getAllWitchVideos,
-}
+  getAllWithVideos,
+  getAll,
+};
